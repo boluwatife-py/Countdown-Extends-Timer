a8e41014dfd61f151b232cdb970d94954b8d9d98
BOLUWATIFE <boluwatife8081@gmail.com>
2025-05-21 15:15:14 +0100

    Updated Codebase: Added Favicon, updated the coutdown label text, fixed main.cpp icon not found issue

---
+++ build/CMakeFiles/TimerServer.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/BOLUWATIFE/Desktop/timer/resource.rc" "C:/Users/BOLUWATIFE/Desktop/timer/build/CMakeFiles/TimerServer.dir/resource.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "UNICODE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "C:/Users/BOLUWATIFE/Desktop/timer"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/BOLUWATIFE/Desktop/timer/main.cpp" "CMakeFiles/TimerServer.dir/main.cpp.obj" "gcc" "CMakeFiles/TimerServer.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TimerServer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TimerServer.dir/main.cpp.obj"
+  "CMakeFiles/TimerServer.dir/main.cpp.obj.d"
+  "CMakeFiles/TimerServer.dir/resource.rc.obj"
+  "TimerServer.exe"
+  "TimerServer.exe.manifest"
+  "TimerServer.pdb"
+  "libTimerServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/TimerServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
